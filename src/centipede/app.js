ddc487da074c7aebcc5d806a174e7ce459757f7e
Oshadha <hpgmoshadhapalliyaguru@gmail.com>
2025-08-15 16:35:58 +0530

    first commit

---
+++ src/centipede/app.js
@@ -0,0 +1,110 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js'; // to read the 3d files
+import { gsap } from 'https://cdn.skypack.dev/gsap';
+
+const camera = new THREE.PerspectiveCamera(
+    10,//view angle 
+    window.innerWidth / window.innerHeight,//ratio of the frame- large as the browser winfow
+    0.1, // closest distance of the camera
+    1000
+);
+camera.position.z = 5;
+
+const scene = new THREE.Scene();
+let bee;
+let mixer;
+const loader = new GLTFLoader();
+loader.load('centipede_test_non-commercial.glb',
+    function (gltf) {//runs when the model loding process complete
+        bee = gltf.scene;
+        scene.add(bee);
+
+        mixer = new THREE.AnimationMixer(bee); // make the model animation
+        mixer.clipAction(gltf.animations[0]).play();
+        modelMove();
+    },
+    function (xhr) {},// make to continuoulsy run
+    function (error) {}
+);
+const renderer = new THREE.WebGLRenderer({alpha: true});// make the canvas
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('container3D').appendChild(renderer.domElement);
+
+// light
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.6);
+scene.add(ambientLight);
+
+const topLight = new THREE.DirectionalLight(0xffffff, 3);
+topLight.position.set(500, 500, 500);
+scene.add(topLight);
+
+
+const reRender3D = () => {
+    requestAnimationFrame(reRender3D);
+    renderer.render(scene, camera);
+    if(mixer) mixer.update(0.02); // make the animation runs continuously
+};
+reRender3D();
+
+let arrPositionModel = [
+    {
+        id: 'banner',
+        position: {x: -0.3, y: -0.2, z: 1},
+        rotation: {x: -0.5, y: -1, z: 0}
+    },
+    {
+        id: "intro",
+        position: { x: 1, y: 0, z: -5 },
+        rotation: { x: 0.5, y: 0.5, z: 0 },
+    },
+    {
+        id: "description",
+        position: { x: -1, y: 0, z: -1 },
+        rotation: { x: 0.7, y: -0.5, z: 0 },
+    },
+    {
+        id: "contact",
+        position: { x: 0.8, y: 0, z: -1 },
+        rotation: { x: 0.3, y: 5, z: 0 },
+    },
+];
+const modelMove = () => {
+    const sections = document.querySelectorAll('.section');
+    let currentSection;
+    sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= window.innerHeight / 3) {
+            currentSection = section.id;
+        }
+    });
+    let position_active = arrPositionModel.findIndex(
+        (val) => val.id == currentSection
+    );
+    if (position_active >= 0) {
+        let new_coordinates = arrPositionModel[position_active];
+        gsap.to(bee.position, { // make the scroll smooth 
+            x: new_coordinates.position.x,
+            y: new_coordinates.position.y,
+            z: new_coordinates.position.z,
+            duration: 3,
+            ease: "power1.out"
+        });
+        gsap.to(bee.rotation, { // gsap roattion of the 3d model
+            x: new_coordinates.rotation.x,
+            y: new_coordinates.rotation.y,
+            z: new_coordinates.rotation.z,
+            duration: 3,
+            ease: "power1.out"
+        })
+    }
+}
+window.addEventListener('scroll', () => {
+    if (bee) {
+        modelMove();
+    }
+})
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
